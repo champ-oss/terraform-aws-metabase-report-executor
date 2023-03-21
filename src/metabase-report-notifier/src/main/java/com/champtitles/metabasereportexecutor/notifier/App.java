d62ef27bb59797d980cb29c2ee8f74803381b07f
Justin Reeves <reeves122@gmail.com>
2023-03-21 11:12:01 -0500

    create java lambda apps

---
+++ src/metabase-report-notifier/src/main/java/com/champtitles/metabasereportexecutor/notifier/App.java
@@ -0,0 +1,16 @@
+package com.champtitles.metabasereportexecutor.notifier;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+
+public class App implements RequestHandler<S3Event, Void> {
+
+    @Override
+    public Void handleRequest(final S3Event event, final Context context) {
+        System.out.println("hello world");
+        System.out.println(event.toString());
+        return null;
+    }
+
+}
