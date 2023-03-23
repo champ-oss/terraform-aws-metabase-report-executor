9d05b9e116846d67d646e2292f4cbfddd7553f96
Justin Reeves <reeves122@gmail.com>
2023-03-23 10:27:56 -0500

    create metabase-report-test module

---
+++ src/metabase-report-test/src/main/java/com/champtitles/metabasereportexecutor/test/App.java
@@ -0,0 +1,33 @@
+package com.champtitles.metabasereportexecutor.test;
+
+import com.champtitles.metabasereportexecutor.executor.MetabaseClient;
+import com.champtitles.metabasereportexecutor.executor.model.SessionPropertiesResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.utils.StringUtils;
+
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class.getName());
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        MetabaseClient metabaseClient = new MetabaseClient("http://localhost:12345", "test@example.com", "1289hdf198hjd192je");
+
+        SessionPropertiesResponse sessionPropertiesResponse = metabaseClient.getSessionProperties();
+        if (StringUtils.isBlank(sessionPropertiesResponse.setupToken())) {
+            logger.info("initial setup has already been completed");
+        } else {
+            logger.info("setup token: {}", sessionPropertiesResponse.setupToken());
+            metabaseClient.completeInitialSetup(sessionPropertiesResponse.setupToken());
+        }
+
+        metabaseClient.loginAndGetSession();
+
+        String cardId = metabaseClient.createCard("test");
+
+        metabaseClient.queryCardGetXlsx(cardId);
+    }
+}
