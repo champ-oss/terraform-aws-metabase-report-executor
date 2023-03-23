c770467fe4b0e2c4db992be09362f6e0a9c9fffc
Justin Reeves <reeves122@gmail.com>
2023-03-23 16:17:39 -0500

    use 35 chars

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 module "s3" {
   source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.36-71b7f67"
-  git     = substr("${var.git}", 0, 37)
+  git     = substr(var.git, 0, 35)
   protect = var.protect
   tags    = merge(local.tags, var.tags)
 }
