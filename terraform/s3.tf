1275661d3f63311d176327a19bc651907296f288
Justin Reeves <reeves122@gmail.com>
2023-03-23 16:16:46 -0500

    set max length of s3 bucket prefix

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 module "s3" {
   source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.36-71b7f67"
-  git     = var.git
+  git     = substr("${var.git}", 0, 37)
   protect = var.protect
   tags    = merge(local.tags, var.tags)
 }
