65432c2bee89b0b61e7c140ca36c569240388cbf
Justin Reeves <reeves122@gmail.com>
2023-03-27 15:45:22 -0500

    open xlsx files for testing

--- src/metabase-report-test/src/main/java/com/champtitles/metabasereportexecutor/test/App.java
+++ src/metabase-report-test/src/main/java/com/champtitles/metabasereportexecutor/test/App.java
@@ -3,17 +3,25 @@ package com.champtitles.metabasereportexecutor.test;
 import com.champtitles.metabasereportexecutor.executor.MetabaseClient;
 import com.champtitles.metabasereportexecutor.executor.model.SessionPropertiesResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class App {
@@ -46,8 +54,14 @@ public class App {
         logger.info("invoking executor lambda");
         invokeExecutorLambda();
 
-        logger.info("checking xlsx file in s3 bucket: {}", bucket);
-        listS3Objects();
+        logger.info("checking xlsx files in s3 bucket: {}", bucket);
+        List<String> objects = listS3Objects();
+        assertTrue(objects.size() > 0);
+
+        for (String s3Key : objects) {
+            assertTrue(getXlsxRowCount(s3Key) >= 200);
+        }
+
     }
 
     private static void invokeExecutorLambda() {
@@ -60,12 +74,37 @@ public class App {
         logger.info("invoke response: {}", invokeResponse.statusCode());
     }
 
-    private static void listS3Objects() {
+    private static List<String> listS3Objects() {
         S3Client s3Client = S3Client.builder().build();
         ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder().bucket(bucket).build();
         ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
+
+        List<String> objects = new ArrayList<String>();
         for (S3Object s3Object : listObjectsV2Response.contents()) {
             logger.info("s3 object: key={} bytes={}", s3Object.key(), s3Object.size());
+            objects.add(s3Object.key());
+        }
+        logger.info("found {} files in s3 bucket: {}", objects.size(), bucket);
+        return objects;
+    }
+
+    private static int getXlsxRowCount(String s3Key) {
+        logger.info("downloading file s3://{}/{}", bucket, s3Key);
+        S3Client s3Client = S3Client.builder().build();
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucket).key(s3Key).build();
+        ResponseInputStream<GetObjectResponse> getObjectResponse = s3Client.getObject(getObjectRequest);
+
+        try {
+            logger.info("opening xlsx file");
+            Workbook workbook = new XSSFWorkbook(getObjectResponse);
+            Sheet sheet = workbook.getSheetAt(0);
+            int rows = sheet.getPhysicalNumberOfRows();
+            logger.info("xlsx file has {} rows", rows);
+            return rows;
+
+        } catch (IOException e) {
+            logger.error("unable to open xlsx file: {}", s3Key);
+            throw new RuntimeException(e);
         }
     }
 }
