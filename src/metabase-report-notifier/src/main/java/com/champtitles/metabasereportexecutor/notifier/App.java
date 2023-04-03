859dcd5620a83765fc31ff9230d3bb5101fa66e1
Justin Reeves <reeves122@gmail.com>
2023-04-03 13:54:44 -0500

    update notifier to parse SNS event

--- src/metabase-report-notifier/src/main/java/com/champtitles/metabasereportexecutor/notifier/App.java
+++ src/metabase-report-notifier/src/main/java/com/champtitles/metabasereportexecutor/notifier/App.java
@@ -3,30 +3,56 @@ package com.champtitles.metabasereportexecutor.notifier;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App implements RequestHandler<SNSEvent, String> {
+import java.util.NoSuchElementException;
+
+public class App implements RequestHandler<SNSEvent, Void> {
 
     private static final Logger logger = LoggerFactory.getLogger(App.class.getName());
     private static final String bucket = System.getenv("BUCKET");
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final JsonPointer objectKeyPtr = JsonPointer.compile("/Records/0/s3/object/key");
     private final S3Reader s3Reader;
 
     public App() {
-        ;
         s3Reader = new S3Reader(bucket);
     }
 
     @Override
-    public String handleRequest(SNSEvent snsEvent, Context context) {
+    public Void handleRequest(SNSEvent snsEvent, Context context) {
         logger.info(snsEvent.toString());
 
-//        for (S3EventNotification.S3EventNotificationRecord record : event.getRecords()) {
-//            byte[] data = s3Reader.downloadXlsx(record.getS3().getObject().getUrlDecodedKey());
-//            logger.info("downloaded {} bytes", data.length);
-//        }
+        for (SNSEvent.SNSRecord snsRecord : snsEvent.getRecords()) {
+            String s3Key = parseS3Key(snsRecord.getSNS().getMessage());
+            byte[] data = s3Reader.downloadXlsx(s3Key);
+            logger.info("downloaded {} bytes", data.length);
+        }
 
         return null;
+    }
+
+    /**
+     * Parse and return the S3 key from an SNS message body
+     *
+     * @param snsMessage body of SNS message containing an S3 event
+     * @return string of S3 key
+     */
+    static String parseS3Key(String snsMessage) {
+        try {
+            logger.info("parsing s3 key from sns message: {}", snsMessage);
+            JsonNode root = mapper.readTree(snsMessage);
+            return root.at(objectKeyPtr).textValue();
+
+        } catch (JsonProcessingException e) {
+            logger.error("failed to parse SNS message: {}", snsMessage);
+            throw new NoSuchElementException(e.getMessage());
+        }
     }
 
 }
