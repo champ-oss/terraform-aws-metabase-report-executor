6e659069655d923f51cfffd0be46c4059093d2c6
Justin Reeves <reeves122@gmail.com>
2023-04-11 15:58:13 -0500

    update s3 terraform module version

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.36-71b7f67"
+  source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.37-d61bfe0"
   git     = substr(var.git, 0, 25)
   name    = "card-${var.metabase_card_id}"
   protect = var.protect
